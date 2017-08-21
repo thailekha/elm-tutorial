85f85929400650713e0a8f440cdd79a22f581faf
thailekha <thailekha@yahoo.com>
2017-08-21 17:32:28 +0100

    fetch data

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -1,5 +1,9 @@
 module Msgs exposing (..)
 
+import Models exposing (Player)
+import RemoteData exposing (WebData)
+
+-- OnFetchPlayers will be called when we get the response from the server. This message will carry a WebData (List Player)
 
 type Msg
-    = NoOp
+    = OnFetchPlayers (WebData (List Player))

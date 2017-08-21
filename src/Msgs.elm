1136d8f7671d6c7d7f34d5239b9fbf4d975db556
thailekha <thailekha@yahoo.com>
2017-08-21 18:35:09 +0100

    hook up router (I)

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -1,9 +1,11 @@
 module Msgs exposing (..)
 
 import Models exposing (Player)
+import Navigation exposing (Location)
 import RemoteData exposing (WebData)
 
 -- OnFetchPlayers will be called when we get the response from the server. This message will carry a WebData (List Player)
 
 type Msg
     = OnFetchPlayers (WebData (List Player))
+    | OnLocationChange Location

970e6e4010f83dd87e94539c0a72e6e38f0f7a36
thailekha <thailekha@yahoo.com>
2017-08-21 18:56:34 +0100

    edit player and save to backend

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -3,9 +3,12 @@ module Msgs exposing (..)
 import Models exposing (Player)
 import Navigation exposing (Location)
 import RemoteData exposing (WebData)
+import Http
 
 -- OnFetchPlayers will be called when we get the response from the server. This message will carry a WebData (List Player)
 
 type Msg
     = OnFetchPlayers (WebData (List Player))
     | OnLocationChange Location
+    | ChangeLevel Player Int
+    | OnPlayerSave (Result Http.Error Player)

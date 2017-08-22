d4c8cb053f4b5429f2d3ae4d6b7a36840ac352e1
thailekha <thailekha@yahoo.com>
2017-08-22 15:03:53 +0100

    implement wordfind lookup

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
 var jsonServer = require('json-server')
+var request = require('superagent');
+var cheerio = require('cheerio')
+
+var url = 'http://www.wordfind.com/contains/';
 
 // Returns an Express server
 var server = jsonServer.create()
@@ -6,8 +10,24 @@ var server = jsonServer.create()
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(jsonServer.defaults())
 
+server.get('/lookupword', function(req, res) {
+    request.get(url + req.query.w, function(err, response) {
+        if (err) throw err;
+        var listItems = cheerio.load(response.text)('li.defLink');
+        var results = [];
+        for (var key in listItems) {
+            if(parseInt(key) >= 0) {
+                results = results.concat(listItems[key].children.map(function(aTag) {
+                    return aTag.children[0].data;
+                }));
+            }
+        }
+        res.json({"wordfind": results});
+    });
+});
+
 var router = jsonServer.router('db.json')
-server.use(router)
+server.use(router);
 
 console.log('Listening at 4000')
 server.listen(4000)
--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,29 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (..)
+import Html.Attributes exposing (placeholder)
+import Html.Events exposing (onInput, onClick)
+import RemoteData exposing (WebData)
+import Http
+import Json.Decode as Decode
+import Json.Decode.Pipeline exposing (decode, required)
 
 
 -- MODEL
 
+type alias Vocab = 
+    { wordfind : List String
+    }
 
 type alias Model =
-    String
+    { content : String
+    , result : WebData (Vocab)
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Hello", Cmd.none )
+    ( { content = "", result = RemoteData.NotAsked }, Cmd.none )
 
 
 
@@ -20,7 +31,9 @@ init =
 
 
 type Msg
-    = NoOp
+    = Change String
+    | Curl
+    | OnResponse (WebData (Vocab))
 
 
 
@@ -29,9 +42,45 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text model ]
+  div []
+    [ input [ placeholder "Text to reverse", onInput Change ] []
+    , button [ onClick Curl ] [ text "Look!" ]
+    , div [] [maybeResult model.result]
+    ]
 
+
+maybeResult : (WebData Vocab) -> Html Msg
+maybeResult response =
+    case response of
+        RemoteData.NotAsked ->
+            text "Look up something ..."
+        RemoteData.Loading ->
+            text "Loading..."
+        RemoteData.Success vocab ->
+            vocabList vocab
+        RemoteData.Failure error ->
+            text (toString error)
+
+
+vocabList : Vocab -> Html Msg
+vocabList vocab =
+    -- notice that 2nd [] here is replaced with (List.map ...)
+    div [] 
+    [
+      h5 [] 
+      [
+        vocab.wordfind 
+            |> List.length
+            |> toString 
+            |> (++) "Found "
+            |> text
+      ]
+    , ul [] 
+      (
+        vocab.wordfind
+            |> List.map (\word -> li [] [text word])
+      )
+    ]
 
 
 -- UPDATE
@@ -40,9 +89,29 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Change newContent ->
+            ({ model | content = newContent }, Cmd.none )
+        Curl ->
+            ({ model | result = RemoteData.Loading }, curl model.content )
+        OnResponse response ->
+            ({ model | result = response }, Cmd.none )
 
+
+-- COMMANDS
+
+
+curl : String -> Cmd Msg
+curl query = 
+    Http.get ("http://localhost:4000/lookupword?w=" ++ query) vocabDecoder
+        |> RemoteData.sendRequest
+        |> Cmd.map OnResponse
+
+
+--https://github.com/NoRedInk/elm-decode-pipeline
+vocabDecoder : Decode.Decoder Vocab
+vocabDecoder =
+    decode Vocab
+        |> required "wordfind" (Decode.list Decode.string)
 
 
 -- SUBSCRIPTIONS
